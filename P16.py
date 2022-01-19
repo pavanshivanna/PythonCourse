c1df9c29461a91c76b1c0a4ddfcd04b163932f97
pkshivanna <pshivanna@qwinix.io>
2022-01-19 18:51:30 +0530

    Basics

---
+++ P16.py
@@ -0,0 +1,6 @@
+#string format
+
+def foo(name):
+    return "Hi %s" % name.title()
+
+print(foo('pavan'))
---
+++ P4.py
@@ -0,0 +1,13 @@
+# Python program to find the largest
+# number among the three numbers
+
+# Driven code
+a = 10
+b = 14
+c = 12
+def maximum(a, b, c):
+    return max(a,b,c)
+
+
+
+print(maximum(a, b, c))
