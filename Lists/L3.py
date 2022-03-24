460f024ee07552e71c7ca771ca349ddf16ff148c
pkshivanna <pshivanna@qwinix.io>
2022-03-24 17:09:19 +0530

    Data str and list programs

---
+++ Lists/L3.py
@@ -0,0 +1,9 @@
+#Turn every item of a list into its square
+
+numbers = [1, 2, 3, 4, 5, 6, 7]
+# result list
+res = []
+for i in numbers:
+    # calculate square and add to the result list
+    res.append(i * i)
+print(res)
---
+++ Lists/L6.py
@@ -0,0 +1,7 @@
+#Remove empty strings from the list of strings
+
+list1 = ["Mike", "", "Emma", "Kelly", "", "Brad"]
+
+# remove None from list1 and convert result into list
+res = list(filter(None, list1))
+print(res)
