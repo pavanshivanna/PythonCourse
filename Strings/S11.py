f143193a6d20f56600a7536ef7761288348c410e
pavanshivanna <98008742+pavanshivanna@users.noreply.github.com>
2022-03-24 17:49:00 +0530

    Delete Strings directory

--- Strings/S11.py
+++
@@ -1,7 +0,0 @@
-#Reverse a given string
-
-str1 = "Pavans"
-print("Original String is:", str1)
-
-str1 = str1[::-1]
-print("Reversed String is:", str1)
--- Strings/S12.py
+++
@@ -1,7 +0,0 @@
-#Find the last position of a given substring
-
-str1 = "Pavan is a data scientist who knows Python. Pavan works at google."
-print("Original String is:", str1)
-
-index = str1.rfind("Pavan")
-print("Last occurrence of Pavan starts at index:", index)
