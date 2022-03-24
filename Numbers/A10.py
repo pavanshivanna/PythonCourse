b2ad3bc5ee34cf23ce1060491ecd5afad86e9913
pavanshivanna <98008742+pavanshivanna@users.noreply.github.com>
2022-03-24 17:48:32 +0530

    Delete Numbers directory

--- Numbers/A10.py
+++
@@ -1,6 +0,0 @@
-#Return the count of a given substring from a string
-
-str_x = "Pavan is good developer. Pavan is a writer"
-# use count method of a str class
-cnt = str_x.count("Pavan")
-print(cnt)
