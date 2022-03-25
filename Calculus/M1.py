8e5d87db974670e897cbc6597b4bb38072e160d5
pkshivanna <pshivanna@qwinix.io>
2022-03-25 12:17:44 +0530

    Arthmatics

---
+++ Calculus/M1.py
@@ -0,0 +1,7 @@
+#program to check whether the given integer is a multiple of both 5 and 7
+
+number = int(input("Enter an integer: "))
+if((number%5==0)and(number%7==0)):
+    print(number, "is a multiple of both 5 and 7")
+else:
+    print(number, "is not a multiple of both 5 and 7")
---
+++ Calculus/M2.py
@@ -0,0 +1,10 @@
+#program to find the average of 10 numbers using while loop
+
+count = 0
+sum = 0.0
+while(count<10):
+    number = float(input("Enter a real number: "))
+    count=count+1
+    sum = sum+number
+avg = sum/10;
+print("Average is :",avg)
